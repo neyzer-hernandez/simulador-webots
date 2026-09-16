a5fee347a712a542e13575d75e114c1d00b2a50a
Neyzer Hernandez <a2233520223@alumnos.uat.edu.mx>
2026-09-15 22:14:30 -0600

    Add files via upload

---
+++ moose/controllers/epuck_adelante/build/release/epuck_adelante.d
@@ -0,0 +1,5 @@
+build/release/epuck_adelante.o: epuck_adelante.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
